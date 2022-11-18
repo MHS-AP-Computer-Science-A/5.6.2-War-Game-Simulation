ed158f07c9d2c991cdb58b12d44bab35b475c7b6
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2022-11-18 15:08:02 -0500

    Update Randomizer.java

--- src/Randomizer.java
+++ src/Randomizer.java
@@ -2,12 +2,12 @@ public class Randomizer {
 	
 	//Post: returns a random from 0-9
 	public static int nextInt() {
-		return (int)(Math.random() * 10);
+		//copy your code from the previous lab
 	}
 
 	//Post: returns a random from start to stop (inclusive)
 	public static int nextInt(int start, int stop) {
-		return (int)(Math.random() * (stop - start + 1) + start);
+		//copy your code from the previous lab
 	}
 	
 }
